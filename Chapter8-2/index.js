ed7773f956f07dcee5c4ad9828d0978f251c8a0d
Maxpan <1315592944@qq.com>
2021-04-06 00:07:27 +0800

    add 8-2

---
+++ Chapter8-2/index.js
@@ -0,0 +1,58 @@
+const electron = require('electron');
+const { app, globalShortcut, screen} = require('electron');
+const url = require('url');
+const path = require('path');
+
+let window = null;
+
+app.whenReady().then(() => {
+  createWindow();
+
+  const result = globalShortcut.register(`Ctrl+0`, function(){
+    window.webContents.send('begin-capture');
+  })
+  if(!result){
+    console.log('注册快捷键失败');
+  }else{
+    console.log('注册快捷键成功');
+  }
+})
+
+
+// app.on('second-instance', (event, commandLine, workingDirectory) => {
+//   if (window) {
+//     if (window.isMinimized()){
+//       window.restore();
+//     }
+//     window.focus();
+//   }
+// })
+
+function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize
+  window = new electron.BrowserWindow({
+    width: width,
+    height: height,
+    show: true, //默认不显示窗口
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    }
+  })
+
+  const urls = url.format({
+    protocol: 'file',
+    pathname: path.join(__dirname, 'index.html')
+  })
+
+  window.loadURL(urls)
+
+  window.on('close', function(){
+    window = null;
+  })
+}
+
+app.on('window-all-closed', function () {
+  app.quit();
+});
---
+++ Chapter8-2/test/index.spec.js
@@ -0,0 +1,58 @@
+const Application = require('spectron').Application
+const chai = require('chai')
+const electronPath = require('electron')
+const path = require('path')
+const fs = require('fs')
+
+chai.should()
+
+describe('SceenShot', function () {
+  this.timeout(10000);
+
+  beforeEach(function () {
+    this.app = new Application({
+      path: electronPath,
+      args: [path.join(__dirname, '../index.js')]
+    })
+    return this.app.start()
+  })
+
+  afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop()
+    }
+  })
+
+  // it('it should create 1 window after app launch', async function () {
+  //   await this.app.client.waitUntilWindowLoaded();
+  //   const winCount = await this.app.client.getWindowCount();
+  //   winCount.should.equal(1);
+  // });
+
+  it('it should create neccessary electron in window', async function () {
+    await this.app.client.waitUntilWindowLoaded();
+
+    const saveBtnElem = await this.app.client.$('#save-btn');
+    const closeBtnElem = await this.app.client.$('#close-btn');
+    const canvasElem = await this.app.client.$('#screen-shot');
+
+    const saveBtnElemIsExist = await saveBtnElem.isExisting();
+    const closeBtnElemIsExist = await closeBtnElem.isExisting();
+    const canvasElemIsExist = await canvasElem.isExisting();
+
+    saveBtnElemIsExist.should.equal(true);
+    closeBtnElemIsExist.should.equal(true);
+    canvasElemIsExist.should.equal(true);
+});
+
+  // it('it should create an screenshot image file in current folder and named screenshot.png', async function () {
+  //   await this.app.client.waitUntilWindowLoaded();
+  //   await this.app.webContents.send('begin-capture');
+  //   const saveBtnElem = await this.app.client.$('#save-btn');
+  //   saveBtnElem.click();
+  //   setTimeout(()=>{
+  //     const isFileExist = fs.existsSync('./screenshot.png');
+  //     isFileExist.should.equal(true);
+  //   }, 2000);
+  // })
+})
