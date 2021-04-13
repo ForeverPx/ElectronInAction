dec2774717cef4bed6d772545ff9582e7a5b7686
panxiao <1315592944@qq.com>
2021-04-13 16:22:47 +0800

    add 8-4

---
+++ Chapter8-4/index.js
@@ -0,0 +1,76 @@
+const electron = require('electron');
+const { app, globalShortcut, screen} = require('electron');
+const url = require('url');
+const path = require('path');
+
+// const { init } = require("@sentry/electron/dist/main");
+
+// init({ 
+//   dsn: "https://b7f8b0ced25349ec907c2acd334c266b@o569388.ingest.sentry.io/5715064"
+// });
+
+const { crashReporter } = require("electron");
+crashReporter.start({
+  companyName: "panxiao",
+  productName: "Demo",
+  ignoreSystemCrashHandler: true,
+  submitURL: "http://127.0.0.1:1127/post",
+});
+
+let window = null;
+
+app.whenReady().then(() => {
+  createWindow();
+
+  const result = globalShortcut.register(`Ctrl+0`, function(){
+    window.webContents.send('begin-capture');
+  })
+  if(!result){
+    console.log('注册快捷键失败');
+  }else{
+    console.log('注册快捷键成功');
+  }
+})
+
+
+// app.on('second-instance', (event, commandLine, workingDirectory) => {
+//   if (window) {
+//     if (window.isMinimized()){
+//       window.restore();
+//     }
+//     window.focus();
+//   }
+// })
+
+function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize
+  window = new electron.BrowserWindow({
+    width: width,
+    height: height,
+    show: false, //默认不显示窗口
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    }
+  })
+
+  const urls = url.format({
+    protocol: 'file',
+    pathname: path.join(__dirname, 'index.html')
+  })
+
+  window.loadURL(urls)
+
+  window.on('close', function(){
+    window = null;
+  })
+}
+
+app.on('window-all-closed', function () {
+  app.quit();
+});
+// myUndefinedFunction();
+// throw new Error('test error')
+
+process.crash();
---
+++ Chapter8-4/window.js
@@ -0,0 +1,83 @@
+const { desktopCapturer, remote, ipcRenderer} = window.require('electron');
+const screen = remote.screen;
+const win = remote.getCurrentWindow();
+const fs = require('fs');
+
+const {
+  drawCanvas,
+  clearCanvas,
+  resizeCanvas
+} = require('./canvas');
+
+new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    reject(22225555);
+  }, 2000);
+})
+
+resizeCanvas();
+drawCanvas();
+
+ipcRenderer.on('begin-capture', function (event) {
+  run();
+});
+
+let nativeImage = null;
+
+async function run() {
+  try {
+    const screenSize = screen.getPrimaryDisplay().workAreaSize;
+    const sources = await desktopCapturer.getSources({
+      types: ['screen'],
+      thumbnailSize: {
+        width: screenSize.width,
+        height: screenSize.height
+      }
+    });
+
+    const entireScreenSource = sources.find(
+      source => source.name === 'Entire Screen' || source.name === 'Screen 1'
+    );
+    nativeImage = entireScreenSource.thumbnail
+      .resize({
+        width: screenSize.width,
+        height: screenSize.height
+      });
+
+    const imageBase64 = nativeImage.toDataURL();
+
+    const img = new Image();
+    img.src = imageBase64;
+    img.onload = function () {
+      const c = document.getElementById("screen-shot");
+      const ctx = c.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      win.show();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const closeBtn = document.getElementById('close-btn');
+closeBtn.addEventListener('click', function(){
+  win.hide();
+  clearCanvas();
+})  
+
+const saveBtn = document.getElementById('save-btn');
+saveBtn.addEventListener('click', function(){
+  // dialog.showOpenDialog(win, {
+  //   properties: ["openDirectory"]
+  // }).then(result => {
+  //   if (result.canceled === false) {
+  //       console.log("Selected file paths:")
+  //       console.log(result.filePaths)
+  //       fs.writeFileSync(`${result.filePaths[0]}/screenshot.png`, nativeImage.toPNG());
+  //   }
+  // }).catch(err => {
+  //   console.log(err)
+  // })
+
+  fs.writeFileSync(`./screenshot.png`, nativeImage.toPNG());
+})  
