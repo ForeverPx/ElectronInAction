8e45d66b53f1899845715ca738167ac35d2e28d9
panxiao <1315592944@qq.com>
2021-05-09 14:23:20 +0800

    remove chapter2 single

--- Chapter2/index.js
+++ Chapter2/index.js
@@ -5,48 +5,33 @@ const path = require('path');
 
 let window = null;
 
-const winTheLock = app.requestSingleInstanceLock();
-if(winTheLock){
-  app.on('second-instance', (event, commandLine, workingDirectory) => {
-    if (window) {
-      if (window.isMinimized()){
-        window.restore();
-      }
-      window.focus();
+function createWindow() {
+  window = new electron.BrowserWindow({
+    width: 600,
+    height: 400,
+    webPreferences: {
+      nodeIntegration: true,
+      webviewTag: true
     }
   })
 
-
-  function createWindow() {
-    window = new electron.BrowserWindow({
-      width: 600,
-      height: 400,
-      webPreferences: {
-        nodeIntegration: true,
-        webviewTag: true
-      }
-    })
-
-    const urls = url.format({
-      protocol: 'file',
-      pathname: path.join(__dirname, 'index.html')
-    })
-
-
-    window.loadURL(urls)
-
-    window.on('close', function(){
-      window = null;
-    })
-  }
-
-  app.on('window-all-closed', function () {
-    app.quit();
+  const urls = url.format({
+    protocol: 'file',
+    pathname: path.join(__dirname, 'index.html')
   })
 
-  app.on('ready', function () {
-    createWindow()
+
+  window.loadURL(urls)
+
+  window.on('close', function(){
+    window = null;
   })
-}else{
-  app.quit();
 }
+
+app.on('window-all-closed', function () {
+  app.quit();
+})
+
+app.on('ready', function () {
+  createWindow()
+})
