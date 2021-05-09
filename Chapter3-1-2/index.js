b1019f9c2ab2475d09415cb7126958c8fda0b6db
panxiao <1315592944@qq.com>
2021-05-09 14:23:30 +0800

    add Chapter3-1-2

---
+++ Chapter3-1-2/index.js
@@ -0,0 +1,52 @@
+const electron = require('electron');
+const app = electron.app;
+const url = require('url');
+const path = require('path');
+
+let window = null;
+
+const winTheLock = app.requestSingleInstanceLock();
+if(winTheLock){
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    if (window) {
+      if (window.isMinimized()){
+        window.restore();
+      }
+      window.focus();
+    }
+  })
+
+
+  function createWindow() {
+    window = new electron.BrowserWindow({
+      width: 600,
+      height: 400,
+      webPreferences: {
+        nodeIntegration: true,
+        webviewTag: true
+      }
+    })
+
+    const urls = url.format({
+      protocol: 'file',
+      pathname: path.join(__dirname, 'index.html')
+    })
+
+
+    window.loadURL(urls)
+
+    window.on('close', function(){
+      window = null;
+    })
+  }
+
+  app.on('window-all-closed', function () {
+    app.quit();
+  })
+
+  app.on('ready', function () {
+    createWindow()
+  })
+}else{
+  app.quit();
+}
